cc53f706b5e200264ee46ef442dda38233befe08
Alex Morega <alex@grep.ro>
2017-03-16 13:56:01 +0200

    create folder for the database

--- ansible/roles/liquid-core/templates/local.py
+++ ansible/roles/liquid-core/templates/local.py
@@ -3,3 +3,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['liquid.local']
 AUTH_PASSWORD_VALIDATORS = []
 CORS_ORIGIN_ALLOW_ALL = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/opt/liquid-core/var/db.sqlite3',
+    }
+}
