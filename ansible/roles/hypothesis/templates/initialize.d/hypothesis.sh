400902166ee15a912f2987dd8aed0ba7946b20f4
Alex Morega <alex@grep.ro>
2017-03-31 13:52:39 +0300

    run hypothesis app

--- ansible/roles/hypothesis/templates/initialize.d/hypothesis.sh
+++ ansible/roles/hypothesis/templates/initialize.d/hypothesis.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
+cd /opt/hypothesis
+
+if [ ! -e libexec/secret_key.sh ]; then
+  echo "export SECRET_KEY='$(openssl rand -base64 32 | tr -d '\n')'" > libexec/secret_key.sh
+fi
+
 sudo -u liquid bash <<EOF
 createdb hypothesis
 psql hypothesis -c 'create extension if not exists "uuid-ossp";'
 
-cd /opt/hypothesis/h
-source ../venv/bin/activate
+source venv/bin/activate
+cd h
 export DATABASE_URL="postgresql:///hypothesis"
 export APP_URL="http://hypothesis.{{ liquid_domain }}"
 export ELASTICSEARCH_HOST="http://127.0.0.1:14312"
