c77e2765ba8c6bfc45438149cf9652a964418fa6
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-04-06 16:56:44 +0300

    add postgres user if it doesn't exist

--- ansible/roles/common/templates/initialize.sh
+++ ansible/roles/common/templates/initialize.sh
@@ -7,7 +7,7 @@ supervisor_up() {
 until supervisor_up; do echo 'waiting for supervisor ...'; sleep 1; done
 echo 'supervisor up'
 
-#sudo -u postgres createuser --superuser liquid
+sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='liquid'" | grep -q 1 || sudo -u postgres createuser --superuser liquid
 
 for file in /opt/common/initialize.d/*
 do
