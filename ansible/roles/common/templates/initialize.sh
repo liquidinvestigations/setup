0a792f4b18fa1e71634c60fb22b1cb5f111fd748
Alex Morega <alex@grep.ro>
2017-04-12 17:36:09 +0300

    wait for supervisor to start (#2)

--- ansible/roles/common/templates/initialize.sh
+++ ansible/roles/common/templates/initialize.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+supervisor_up() {
+  ! supervisorctl status | grep -q 'supervisor\.sock no such file'
+}
+until supervisor_up; do echo 'waiting for supervisor ...'; sleep 1; done
+echo 'supervisor up'
+
 #sudo -u postgres createuser --superuser liquid
 
 for file in /opt/common/initialize.d/*
