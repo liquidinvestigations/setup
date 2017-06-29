278192fe5f28c3bc68b9235eda72d08ba88603a6
Alex Morega <alex@grep.ro>
2017-06-29 18:40:42 +0300

    move scripts from `buildbot/scripts`

---
+++ bin/build_odroid_image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+###
+# Build a Liquid image and save it to `shared/output`
+###
+
+set -e
+
+SETUPDIR=/tmp/liquid-setup
+TARGET=/mnt/target
+TEMPDIR=/tmp
+OUTPUT=/mnt/shared/output
+
+set -x
+
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible git pv
+
+git clone https://github.com/liquidinvestigations/setup $SETUPDIR
+
+curl https://liquidinvestigations.org/images/ubuntu64-16.04-minimal-odroid-c2-20160815-4G.img.xz | xzcat > $TEMPDIR/odroid-c2.img
+
+losetup /dev/loop0 $TEMPDIR/odroid-c2.img -o 135266304
+mkdir -p $TARGET
+mount /dev/loop0 $TARGET
+mount --bind /proc $TARGET/proc
+echo "nameserver 8.8.8.8" > $TARGET/etc/resolv.conf
+
+chroot $TARGET apt-get update
+chroot $TARGET apt-get install -y python
+chroot $TARGET apt-get clean
+
+cd $SETUPDIR/ansible
+touch vars/config.yml
+ansible-playbook board_chroot.yml
+
+umount $TARGET/proc
+umount $TARGET
+losetup -d /dev/loop0
+
+mkdir -p $OUTPUT
+pv < $TEMPDIR/odroid-c2.img | xz -0 > $OUTPUT/odroid-c2-liquid.img.xz
---
+++ bin/build_x86_64_image.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+###
+# Build a Liquid image and save it to `shared/output`
+###
+
+set -e
+
+SETUPDIR=/tmp/liquid-setup
+TARGET=/mnt/target
+TEMPDIR=/tmp
+OUTPUT=/mnt/shared/output
+IMAGE=/mnt/shared/ubuntu-x86_64-raw.img
+
+set -x
+
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible git pv qemu-utils
+
+git clone https://github.com/liquidinvestigations/setup $SETUPDIR
+
+curl https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img > /mnt/shared/ubuntu-x86_64-cow2.img
+qemu-img convert -f qcow2 -O raw /mnt/shared/ubuntu-x86_64-cow2.img $IMAGE
+
+truncate -s 4G $IMAGE
+# This assumes there is only one partition in the image.
+# Keep start sector and type, but use all sectors up to the end of the image.
+sfdisk -d $IMAGE | sed 's/size=[^,]\+,//' | sfdisk $IMAGE
+
+losetup /dev/loop0 $IMAGE -o 1048576
+resize2fs /dev/loop0
+mkdir -p $TARGET
+mount /dev/loop0 $TARGET
+mount --bind /proc $TARGET/proc
+mount --bind /dev $TARGET/dev
+rm -f $TARGET/etc/resolv.conf
+echo "nameserver 8.8.8.8" > $TARGET/etc/resolv.conf
+
+chroot $TARGET apt-get update
+chroot $TARGET apt-get install -y python
+chroot $TARGET apt-get clean
+
+cd $SETUPDIR/ansible
+touch vars/config.yml
+ansible-playbook board_chroot.yml
+
+# console= setting referencing non-existant port can cause hangs during boot:
+# https://bugs.launchpad.net/cloud-images/+bug/1573095
+sed -i 's/console=hvc0 *//g' $TARGET/boot/grub/menu.lst
+
+umount $TARGET/proc
+umount $TARGET/dev
+umount $TARGET
+losetup -d /dev/loop0
+
+mkdir -p $OUTPUT
+pv < $IMAGE | xz -0 > $OUTPUT/x86_64-liquid.img.xz
