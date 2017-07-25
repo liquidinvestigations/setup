724e725c89c413dab205b9f79ff02f52cb2804fc
Alex Morega <alex@grep.ro>
2017-07-25 14:39:34 +0300

    use non-US spelling
    because fuck the current "fake news" rhetoric

--- py/images/cmd.py
+++ py/images/cmd.py
@@ -3,7 +3,7 @@ from .builders.cloud import Builder_cloud
 from .builders.odroid import Builder_odroid_c2, Builder_odroid_xu4
 from . import tools
 
-FLAVORS = {
+FLAVOURS = {
     'cloud': Builder_cloud,
     'odroid_c2': Builder_odroid_c2,
     'odroid_xu4': Builder_odroid_xu4,
@@ -12,9 +12,9 @@ FLAVORS = {
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('flavor', choices=FLAVORS.keys())
+    parser.add_argument('flavor', choices=FLAVOURS.keys())
     parser.add_argument('-d', '--debug', action='store_true')
     options = parser.parse_args()
-    builder_cls = FLAVORS[options.flavor]
+    builder_cls = FLAVOURS[options.flavor]
     tools.DEBUG = options.debug
     builder_cls().build()
