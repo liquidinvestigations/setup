ddb9a80e1a7ab0828110ad4112c314fcb8c48366
Alex Morega <alex@grep.ro>
2017-07-30 03:07:29 +0300

    reverse proxy requests to davros

--- ansible/roles/davros/files/authproxy/authproxy.py
+++ ansible/roles/davros/files/authproxy/authproxy.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+from werkzeug.wsgi import responder
+from werkzeug.wrappers import Request
+from paste.proxy import Proxy
 import flask
 
-app = flask.Flask(__name__)
+proxy_app = Proxy('http://127.0.0.1:51358')
 
-@app.route('/')
+auth_app = flask.Flask(__name__)
+
+
+@auth_app.route('/__auth/')
 def home():
-    return 'hi'
+    return "login goes here"
+
+
+@responder
+def dispatch(environ, start_response):
+    request = Request(environ, shallow=True)
+
+    if request.path.startswith('/__auth/'):
+        return auth_app
+
+    return proxy_app
+
 
 if __name__ == '__main__':
     import waitress
-    waitress.serve(app, host='127.0.0.1', port=32437)
+    waitress.serve(dispatch, host='127.0.0.1', port=32437)
