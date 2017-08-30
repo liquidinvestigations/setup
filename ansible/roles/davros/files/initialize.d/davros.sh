a1a360a26a284fd507b1376c36c55c4feb9c2cb0
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-08-30 15:25:51 +0300

    configure davros with oauth keys

---
+++ ansible/roles/davros/files/initialize.d/davros.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo -u liquid bash <<EOF
+source /var/lib/liquid/oauth_keys/davros
+echo "LIQUID_CLIENT_ID = '$CLIENT_ID'" > /opt/davros/authproxy/config/oauth.py
+echo "LIQUID_CLIENT_SECRET = '$CLIENT_SECRET'" >> /opt/davros/authproxy/config/oauth.py
+EOF
