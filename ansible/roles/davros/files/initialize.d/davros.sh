5fc30d8ddb71710c3db7c2ed71db6b3bdb418a5a
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-08-30 17:03:48 +0300

    fix davros oauth process

--- ansible/roles/davros/files/initialize.d/davros.sh
+++ ansible/roles/davros/files/initialize.d/davros.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo -u liquid bash <<EOF
+set -ex
 source /var/lib/liquid/oauth_keys/davros
 echo "LIQUID_CLIENT_ID = '$CLIENT_ID'" > /opt/davros/authproxy/config/oauth.py
 echo "LIQUID_CLIENT_SECRET = '$CLIENT_SECRET'" >> /opt/davros/authproxy/config/oauth.py
-EOF
