2a653e3c81a4eb7f1cfb1dd4bef30d3caa370677
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-08-31 12:10:14 +0300

    don't echo the private keys

--- ansible/roles/davros/files/initialize.d/davros.sh
+++ ansible/roles/davros/files/initialize.d/davros.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 source /var/lib/liquid/oauth_keys/davros
 echo "LIQUID_CLIENT_ID = '$CLIENT_ID'" > /opt/davros/authproxy/config/oauth.py
 echo "LIQUID_CLIENT_SECRET = '$CLIENT_SECRET'" >> /opt/davros/authproxy/config/oauth.py
