bc1714c2436ec965a652188471854018d83cb267
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-09-11 16:17:38 +0300

    create davros secret key on first boot

--- ansible/roles/davros/templates/authproxy/config/secret.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = "{{ davros_authproxy_secret_key }}"
--- ansible/roles/davros/templates/initialize.d/davros.sh
+++ ansible/roles/davros/templates/initialize.d/davros.sh
@@ -6,3 +6,7 @@ sudo -u liquid /opt/liquid-core/libexec/create-oauth-application "davros" "{{ ht
 source /var/lib/liquid/oauth_keys/davros
 echo "LIQUID_CLIENT_ID = '$CLIENT_ID'" > /opt/davros/authproxy/config/oauth.py
 echo "LIQUID_CLIENT_SECRET = '$CLIENT_SECRET'" >> /opt/davros/authproxy/config/oauth.py
+
+if [ ! -s /opt/davros/authproxy/config/secret.py ]; then
+    echo "SECRET_KEY = '`openssl rand -base64 48`'" > /opt/davros/authproxy/config/secret.py
+fi
