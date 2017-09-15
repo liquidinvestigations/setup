fffbdecbf43bf5ecbcc5d58010cd0b51e8ea5e57
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-09-15 11:27:26 +0300

    add on-boot hook

--- ansible/roles/common/templates/boot.sh
+++ ansible/roles/common/templates/boot.sh
@@ -8,4 +8,15 @@ if [ ! -f first_boot_done ]; then
   echo "First boot completed successfully"
 fi
 
+echo "Starting firewall"
+/opt/common/libexec/firewall
+
+echo "Starting all services"
+# TODO: only start enabled services
 supervisorctl start all
+
+echo "Running on-boot hook"
+for file in /opt/common/hooks/on-boot.d/*
+do
+  "$file"
+done
