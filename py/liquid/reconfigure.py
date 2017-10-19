d40a6309351e2b677595347d12380297358584a4
Alex Morega <alex@grep.ro>
2017-10-19 13:48:57 +0000

    run initialize; reload nginx; restart all services

--- py/liquid/reconfigure.py
+++ py/liquid/reconfigure.py
@@ -1,10 +1,15 @@
 import sys
 import json
 from pathlib import Path
+import subprocess
 from images.setup import install
 from .wifi import configure_wifi
 
 ANSIBLE_VARS = Path(__file__).parent.parent.parent / 'ansible' / 'vars'
+
+def run(cmd):
+    print('+', cmd)
+    subprocess.run(cmd, shell=True, check=True)
 
 
 def on_reconfigure():
@@ -20,6 +25,11 @@ def on_reconfigure():
         print(json.dumps(vars, indent=2, sort_keys=True), file=f)
 
     install(tags='configure')
+    run('/opt/common/initialize.sh')
 
     print('configure_wifi')
     configure_wifi(vars)
+
+    run('service nginx reload')
+    run('supervisorctl update')
+    run('supervisorctl restart all')
