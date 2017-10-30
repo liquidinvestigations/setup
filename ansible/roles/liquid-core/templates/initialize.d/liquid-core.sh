02c050b14002ed94b1fe9b4bf02dfbc5438c66a4
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-10-30 16:23:17 +0200

    fix bash if damnit, episode 2

--- ansible/roles/liquid-core/templates/initialize.d/liquid-core.sh
+++ ansible/roles/liquid-core/templates/initialize.d/liquid-core.sh
@@ -4,12 +4,10 @@ set -e
 printf '\n\n=== INITIALIZE LIQUID-CORE ===\n\n'
 set -x
 
-if [ -z $(grep -q -- "-- placeholder --" /opt/liquid-core/liquid-core/liquidcore/site/settings/secret_key.py) ]; then
-(
+if grep -qs -- '-- placeholder --' /opt/liquid-core/liquid-core/liquidcore/site/settings/secret_key.py; then
     # create secret keys without echoing
     set +x
     echo "SECRET_KEY = '`openssl rand -base64 48`'" > /opt/liquid-core/liquid-core/liquidcore/site/settings/secret_key.py
-)
 fi
 
 cd /opt/liquid-core/liquid-core
