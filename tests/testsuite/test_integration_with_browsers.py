2063881cb09e4b7f0c1c13f68bf43735e0a3c602
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-11-16 18:01:47 +0200

    wait after clicks so the loading screen dissapears

--- tests/testsuite/test_integration_with_browsers.py
+++ tests/testsuite/test_integration_with_browsers.py
@@ -1,4 +1,4 @@
-import os
+import time
 import splinter
 import pytest
 
@@ -32,24 +32,24 @@ BROWSER_OPTS = {
 }
 
 
-WELCOME_FILE_PATH = '/var/lib/liquid/core/welcome_done'
-
-def skip_if_welcome_not_set():
-    if not os.path.isfile(WELCOME_FILE_PATH):
-        pytest.skip('welcome not set')
+def skip_if_welcome_not_set(browser):
+    browser.visit(URL)
+    if browser.url.endswith('/welcome/'):
+        pytest.skip('welcome done')
 
 
 @pytest.fixture(params=['firefox', 'chrome'])
 def browser(request):
     browser_name = request.param
     with splinter.Browser(browser_name, headless=True, wait_time=10, **BROWSER_OPTS[browser_name]) as browser:
+        browser.driver.set_window_size(1920, 1080)
         browser.visit(URL)
         yield browser
 
 
 @pytest.mark.parametrize('browser', ['firefox'], indirect=True)
 def test_browser_welcome(browser):
-    assert not os.path.isfile(WELCOME_FILE_PATH)
+    assert browser.url.endswith('/welcome/')
 
     assert browser.is_text_present("Liquid Investigations")
     assert browser.is_text_present("Congratulations")
@@ -65,11 +65,12 @@ def test_browser_welcome(browser):
     assert browser.is_text_present("Your settings are being applied")
     assert browser.is_text_present("Wait a minute")
 
-    assert os.path.isfile(WELCOME_FILE_PATH)
+    browser.visit(URL)
+    assert not browser.url.endswith('/welcome/')
 
 
 def test_view_home_page(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
 
     for app_name in APP_NAMES:
@@ -77,7 +78,7 @@ def test_view_home_page(browser):
 
 
 def test_login_into_home_page(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
 
     # login
@@ -99,7 +100,7 @@ def test_login_into_home_page(browser):
 
 
 def test_login_into_dokuwiki(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
     
     # navigate to dokuwiki and login
@@ -119,7 +120,7 @@ def test_login_into_dokuwiki(browser):
 
 
 def test_login_into_hypothesis(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
     
     # navigate to hypothesis and login
@@ -135,7 +136,7 @@ def test_login_into_hypothesis(browser):
 
 
 def test_login_into_matrix(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
     
     # navigate to matrix and login
@@ -155,7 +156,7 @@ def test_login_into_matrix(browser):
 
 
 def test_login_into_davros(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
     
     # navigate to davros and login
@@ -170,7 +171,7 @@ def test_login_into_davros(browser):
 
 
 def test_login_into_hoover(browser):
-    skip_if_welcome_not_set()
+    skip_if_welcome_not_set(browser)
     assert browser.is_text_present("Liquid Investigations")
 
     # login
@@ -205,82 +206,141 @@ def test_login_into_hoover(browser):
     browser.find_by_id('loggedin-btngroup').click()
     assert browser.is_text_present("login")
 
-# no chrome on admin UI because clickin issues in menu
-@pytest.mark.parametrize('browser', ['firefox'], indirect=True)
-def test_navigation_through_admin(browser):
-    skip_if_welcome_not_set()
-    assert browser.is_text_present("Liquid Investigations")
+
+def navigate_to_admin(browser):
+    skip_if_welcome_not_set(browser)
 
     # login
     browser.find_by_text('[login]').click()
     browser.fill('username', ADMIN_USERNAME)
     browser.fill('password', ADMIN_PASSWORD)
     browser.find_by_text('login').click()
-
     browser.find_by_text('[admin]').click()
 
-    # we're in the admin now
+
+def test_admin_header_and_redirect_to_status(browser):
+    navigate_to_admin(browser)
+
     assert browser.is_text_present("admin")
     assert browser.is_text_present("Logged in as: {}".format(ADMIN_USERNAME))
 
-    # click on all the buttons
+
+def test_admin_general_status_tab(browser):
+    navigate_to_admin(browser)
+    assert browser.url.endswith("/admin-ui/status")
     browser.click_link_by_href('/admin-ui/status')
+
+    assert browser.url.endswith("/admin-ui/status")
     assert browser.is_text_present("General Status")
 
+
+def test_admin_network_status_tab(browser):
+    navigate_to_admin(browser)
     browser.click_link_by_href('/admin-ui/network')
+    assert browser.url.endswith('/admin-ui/network/status')
+
     assert browser.is_text_present("Network Configuration")
     assert browser.is_text_present("Domain")
     assert browser.is_text_present(DOMAIN)
     assert browser.is_text_present("Lan configuration")
     assert browser.is_text_present(HOTSPOT_SSID)
 
+
+def test_admin_network_lan_tab(browser):
+    navigate_to_admin(browser)
+    browser.click_link_by_href('/admin-ui/network')
+    time.sleep(3)
     browser.click_link_by_href('/admin-ui/network/lan')
+
     # TODO test these; they don't show up as text because they're inside inputs
     #assert browser.is_text_present(HOTSPOT_SSID)
     #assert browser.is_text_present(HOTSPOT_PASSWORD)
-    assert browser.is_text_present('Use Ethernet on LAN')
+    assert browser.is_text_present('DHCP Range')
+    assert browser.is_text_present('Netmask')
 
+
+def test_admin_network_wan_tab(browser):
+    navigate_to_admin(browser)
+    browser.click_link_by_href('/admin-ui/network')
+    time.sleep(3)
     browser.click_link_by_href('/admin-ui/network/wan')
+
     assert browser.is_text_present('DHCP')
-    assert browser.is_text_present('Gateway')
+    #assert browser.is_text_present('Gateway')
     assert browser.is_text_present('DNS Server')
 
-    browser.click_link_by_href('/admin-ui/network/ssh')
-    assert browser.is_text_present('SSH')
-    assert browser.is_text_present('Port')
-    assert browser.is_text_present('Update')
 
+def test_admin_network_ssh_tab(browser):
+    navigate_to_admin(browser)
+    browser.click_link_by_href('/admin-ui/network')
+    time.sleep(3)
+    browser.click_link_by_href('/admin-ui/network/ssh')
+
+    assert browser.is_text_present('SSH')
+    #assert browser.is_text_present('Port')
+
+
+def test_admin_vpn_status_tab(browser):
+    navigate_to_admin(browser)
     browser.click_link_by_href('/admin-ui/vpn')
+    assert browser.url.endswith('/admin-ui/vpn/status')
+
     assert browser.is_text_present("VPN Configuration")
     assert browser.is_text_present("Server configuration")
     assert browser.is_text_present("Client configuration")
     assert browser.is_text_present("Connection count")
 
+
+def test_admin_vpn_server_tab(browser):
+    navigate_to_admin(browser)
+    browser.click_link_by_href('/admin-ui/vpn')
+    time.sleep(3)
     browser.click_link_by_href('/admin-ui/vpn/server')
-    assert browser.is_text_present("Enable VPN server")
+    #assert browser.is_text_present("Enable VPN server")
     assert browser.is_text_present("Generate new key")
     assert browser.is_text_present("Active keys")
 
-    browser.click_link_by_href('/admin-ui/vpn/client')
-    assert browser.is_text_present("Enable VPN client")
-    assert browser.is_text_present("Upload key")
 
+def test_admin_vpn_client_tab(browser):
+    navigate_to_admin(browser)
+    browser.click_link_by_href('/admin-ui/vpn')
+    time.sleep(3)
+    browser.click_link_by_href('/admin-ui/vpn/client')
+    #assert browser.is_text_present("Enable VPN client")
+    #assert browser.is_text_present("Upload key")
+
+
+def test_admin_services_tab(browser):
+    navigate_to_admin(browser)
     browser.click_link_by_href('/admin-ui/services')
+    time.sleep(3)
     assert browser.is_text_present("Services")
     for app_name in APP_NAMES:
         assert browser.is_text_present(app_name.upper())
     for app_desc in ['Search Tool', 'Annotations', 'Chat', 'Wiki', 'File Sharing']:
         assert browser.is_text_present(app_desc)
 
+
+def test_admin_users_tab(browser):
+    navigate_to_admin(browser)
     browser.click_link_by_href('/admin-ui/users')
+
     assert browser.is_text_present("Users")
     assert browser.is_text_present("Active users")
     assert browser.is_text_present("Inactive users")
 
+
+def test_admin_discovery_tab(browser):
+    navigate_to_admin(browser)
     browser.click_link_by_href('/admin-ui/discovery')
+
     assert browser.is_text_present("Discovery")
     assert browser.is_text_present("Trusted nodes")
     assert browser.is_text_present("Untrusted nodes")
+
+
+def test_admin_about_tab(browser):
+    navigate_to_admin(browser)
 
     browser.click_link_by_href('/admin-ui/about')
     assert browser.is_text_present("Lorem ipsum dolor sit amet")
