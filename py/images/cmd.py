68b318fe66d5521d54e46443cdd7a916ee974480
Alex Morega <alex@grep.ro>
2017-11-27 17:01:26 +0200

    `with-image-chroot` command

--- py/images/cmd.py
+++ py/images/cmd.py
@@ -1,6 +1,8 @@
-from argparse import ArgumentParser
+import sys
+from argparse import ArgumentParser, REMAINDER
 from . import setup
 from . import tools
+from .builders.cloud import Builder_cloud
 
 
 def build_image():
@@ -19,3 +21,15 @@ def install():
     parser.add_argument('--tags', default=None)
     options = parser.parse_args()
     setup.install(options.tags)
+
+
+def run_with_image_chroot():
+    parser = ArgumentParser()
+    parser.add_argument('image', help="Path to the image")
+    parser.add_argument('cmd', nargs=REMAINDER)
+    options = parser.parse_args()
+
+    builder = Builder_cloud()
+    with builder.open_target(options.image) as target:
+        print('+', ' '.join(options.cmd), file=sys.stderr)
+        target.chroot_run(options.cmd)
